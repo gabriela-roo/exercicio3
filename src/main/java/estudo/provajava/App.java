1125f5f664782a90bb9c823201c43ed9f6906c76
gabriela-roo <144496434+gabriela-roo@users.noreply.github.com>
2024-04-18 13:43:55 -0300

    first commit

---
+++ src/main/java/estudo/provajava/App.java
@@ -0,0 +1,13 @@
+package estudo.provajava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/estudo/provajava/AppTest.java
@@ -0,0 +1,20 @@
+package estudo.provajava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
